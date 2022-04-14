7ac741a7dcd41f76eea5c89aad5b33328bee4170
yoshi70001 <surke@outlook.es>
2022-04-13 23:58:16 -0500

    asssss

---
+++ app.js
@@ -0,0 +1,13 @@
+let theme=false;
+function cargar(){
+    var uwu=new Promise(function(resolve,error){
+        
+    });
+}
+function mostrar(){
+    
+}
+function color_theme(){
+    theme=!theme
+    
+}
